527a88cb82cba99e11b09bac321c7b6c91054bb5
Brice Perez <brice@kperez.me>
2016-03-25 04:49:27 -0500

    remove Drupal files
    Old Drupal site removed

--- brice/sites/all/themes/cvpro/tpl/comment-wrapper.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if ($content['#node']->comment and !($content['#node']->comment == 1 and $content['#node']->comment_count)) { ?>
-		<h4><?php print t('Comments').' ('.$content['#node']->comment_count.')';?></h4>
-		<?php print render($content['comments']); ?>
-		<?php print str_replace('resizable', '', render($content['comment_form'])); ?>
-		<?php
-	}
-?>
--- brice/sites/all/themes/cvpro/tpl/footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<footer>
-	<?php $footer_copyright = theme_get_setting('footer_copyright_message', 'cvpro'); ?>
-	<?php if(!empty($footer_copyright)) :?>
-		<h6><?php print $footer_copyright ?></h6>
-	<?php endif; ?>
-</footer>
